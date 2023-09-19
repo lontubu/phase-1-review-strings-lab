83e37715269dc3b5c2d21c214189d5c638de30a0
lontubu <nickson.lontubu@studentmoringa.com>
2023-09-19 15:46:15 +0300

    assingment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hooper';
+
+const welcomeMessage = 'Welcome to Flatbook, $[Grace Hooper]!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome, $(G,slice[0, 1])!';
